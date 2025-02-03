ce05bf1f814a060b8d9d20177eb79572eed883f8
DemouraGus <demoura.gus@gmail.com>
2025-02-03 12:52:54 -0800

    car_listings controller updated to include images for each listing

--- Backend/app/controllers/car_listings_controller.rb
+++ Backend/app/controllers/car_listings_controller.rb
@@ -1,7 +1,14 @@
 class CarListingsController < ApplicationController
 
   def index
-    @car_listings = CarListing.all
-    render json: @car_listings
+    @car_listings = CarListing.includes(:images).all
+    
+    render json: @car_listings.map { |car_listing| 
+      car_listing.as_json.merge(
+        images: car_listing.images.map(&:url)
+      )
+    }
   end 
+
+  
 end
--- Backend/app/models/car_listing.rb
+++ Backend/app/models/car_listing.rb
@@ -1,4 +1,4 @@
 class CarListing < ApplicationRecord
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
 end
