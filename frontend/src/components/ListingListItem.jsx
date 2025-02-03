84332ce39ed36a5d3350c6c0f628878544d77cc4
Saurav Dhillon <souravdhillon010@gmail.com>
2025-02-03 01:49:04 -0500

    Updated ListingListItem component to render individual listitem to frontend using mock data and created mock folder to hold mockCarData.js

---
+++ frontend/src/components/ListingListItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "../styles/ListingListItem.scss";
+
+const ListingListItem = ({ car }) => {
+
+  return (
+    <li className="listing-list__item">
+    <img className="listing-list__image"
+        src={car.image}
+        alt={`${car.make} ${car.model}`}
+      />
+
+      <div className="listing-list__car-details">
+        <h3>{car.make} {car.model}</h3>
+        <p>Year: {car.year}</p>
+        <p>Mileage: {car.mileage} km</p>
+        <p>Price: {car.price}</p>
+        <p>Location: {car.city}</p>
+      </div>
+    </li>
+  );
+
+};
+
+export default ListingListItem;
---
+++ frontend/src/mocks/mockCarData.js
@@ -0,0 +1,12 @@
+const mockCarData = {
+  id: 1,
+  make: "Toyota",
+  model: "Corolla",
+  year: 2020,
+  mileage: 30000,
+  price: 20000, // Converted from cents to dollars
+  city: "Toronto",
+  image: "https://pr7a1ybbux.ufs.sh/f/yMDF6NTAbMasezNYUj1qED1B7iKjFg2Yw9aPTxyLfvCzUtIc"
+};
+
+export default mockCarData;
